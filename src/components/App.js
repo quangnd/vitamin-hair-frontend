40173e9763fbc88ad192305f641df09e35226780
Quang Nguyen <quang.nguyen@launchdeckpowergate.com>
2017-10-08 13:49:56 +0700

    Edit App.js content

--- src/components/App.js
+++ src/components/App.js
@@ -36,7 +36,7 @@ class App extends Component {
         <div className="container">
           <div className="jumbotron">
             <h1>Welcome to Vitamin hair's site</h1>
-            <p className="lead">We are here to help you.</p>
+            <p className="lead">We are here to help you and make the better life!</p>
             <a className="btn btn-lg btn-primary" href="#" role="button">Read more &raquo;</a>
           </div>
         </div>
