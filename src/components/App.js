2a0f0e03eec3fdab10d0acda95643cec3d907bf4
Nguyen Dang Quang <quangnd.edu@gmail.com>
2017-10-08 13:19:09 +0700

    Add reactstrap, bootstrap, jquery and edit Webpack config (#1)

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
-	render() {
-		return <div>Hello world! I'm vitamin hair!</div>;
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <a className="navbar-brand" href="#">Fixed navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+            <form className="form-inline mt-2 mt-md-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="jumbotron">
+            <h1>Navbar example</h1>
+            <p className="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser's viewport.</p>
+            <a className="btn btn-lg btn-primary" href="../../components/navbar/" role="button">View navbar docs &raquo;</a>
+          </div>
+        </div>
+      </div>
+    );
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
 import App from './components/App';
 
 ReactDOM.render(
